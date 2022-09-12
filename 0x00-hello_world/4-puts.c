e3273c0672301d56d5bf9f6f97e0c8be11e06d99
TshidisoMp3 <tshidisomorake18@gmail.com>
2022-09-12 16:53:34 -0700

    C File Print using Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print - print the string in the puts function
- * 
- * Description: using the main function
- * this progarm prints "Programming is like building a multilingual puzzle
- * Return: 0
- */
- int main(void)
- {
-         
-         puts("\"Programming is like building a multilingual puzzle");
- }
+* main - print the string in the put function
+*
+* Description: using the main fuction
+* this program prints "Programming is a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+
+        puts("\"Programming is a multilingual puzzle");
+        Return (0);
+}
