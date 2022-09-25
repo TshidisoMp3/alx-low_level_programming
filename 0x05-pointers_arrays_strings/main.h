31c8dd3b23bc1a654812fa4b4d826abbe87a1668
TshidisoMp3 <tshidisomorake18@gmail.com>
2022-09-25 16:17:18 -0700

    task 7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-/**
-* void prototypes()
-* int prototypes()
-*/
-
+void _putchar (char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,4 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif /*MAIN_H*/
+#endif
