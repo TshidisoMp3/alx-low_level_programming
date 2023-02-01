547b2d38c9d1e6d316be72dbaee5d4480a5608c9
TshidisoMp3 <tshidisomorake18@gmail.com>
2023-01-31 18:04:05 -0800

    variables, if/else and while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	return (0);
+}
