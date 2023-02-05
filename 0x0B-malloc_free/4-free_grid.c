739dd671fd6b683de8d36ddc9b2c5fac794f4fe2
TshidisoMp3 <tshidisomorake18@gmail.com>
2023-02-05 11:16:57 -0800

    malloc and free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,12 +9,14 @@
  * Return: nothing, frees memory
  */
 
-void free_grid(int **grid, int height)
+void  free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
-		free(grid[i]);
+	{
+		free(grid[i]);`
+	}
 
 	free(grid);
 }
