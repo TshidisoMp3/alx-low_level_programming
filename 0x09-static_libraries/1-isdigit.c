443237dbe8bfd29d0bddd7b932408d97e50ddc01
TshidisoMp3 <tshidisomorake18@gmail.com>
2023-10-22 20:45:22 +0200

    C_StaticLibraries

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -6,7 +6,11 @@
  *Return: 1 if c is a digit, 0 otherwise
  */
 
-int _isdigit(int c);
+
+int _isdigit(int c)
 {
-      return(c>='0'&&c<='9');
+   if (c >= '0' && c <= '9')
+      return (1);
+   else
+      return (0);
 }
