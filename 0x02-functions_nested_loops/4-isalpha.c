41d94f36693fc27e2018dd866a8cc40a4e5ce436
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-02-26 12:58:33 +0200

    C_Nested_Loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - function to check if c is a letter, lowercase or uppercase
+ * _isalpha - helps to check for alphabetic character
  * @c: is the int that will use for the argument of the function
  * Return: 0
  */
