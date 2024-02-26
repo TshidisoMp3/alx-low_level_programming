4fb78f6cc38bdd2c8bb9d02a4eec18f7d559c669
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-02-26 13:56:49 +0200

    C_Nested_Loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,24 +6,19 @@
  */
 
 void generateFibonacci(int n) {
-    int i;
-    long int a = 0, b = 1, c = 0;
-    
-    for (i = 0; i < n; i++) {
-        if (i == 0) {
-        printf("%ld", a);
-        } else if (i == 1) {
-        printf(", %ld", b);
-        } else {
-        c = a + b;
-        a = b;
-        b = c;
-        printf(", %ld", c);
-        }
-    }
-    printf("\n");
-}
+    int first = 1, second = 2, next;
 
+    for (int i = 0; i < n; i++) {
+        if (i <= 1) {
+            next = i;
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d\n", next);
+    }
+}
 int main(void)
 {
     generateFibonacci(50);
